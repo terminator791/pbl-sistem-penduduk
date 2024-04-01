c7898a85ffc75735b7360201c545cde1074bdb29
haqiartanto <maulanaartanto04@gmail.com>
2024-04-01 12:09:18 +0700

    latihan buat haqi

---
+++ app/Models/kesehatan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kesehatan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kesehatan';
+
+    protected $guarded = ['id'];
+
+    public function penduduk(){
+        return $this->belongsTo(penduduk::class, 'NIK_penduduk', 'NIK');
+    }
+
+    public function jenis_penyakit(){
+        return $this->belongsTo(jenis_penyakit::class, 'id_penyakit');
+    }
+}
+//contoh gais yah
